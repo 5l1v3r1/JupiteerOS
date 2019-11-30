da4f9c85a676259ee6da4f903a4acf615ea447b1
furkanonder <furkantahaonder@gmail.com>
2019-11-30 23:20:30 +0300

    Fix integer types

--- src/int_types.h
+++ src/int_types.h
@@ -1,13 +1,13 @@
-#ifndef INCLUDE_UINT_H
-#define INCLUDE_UINT_H
+#ifndef INCLUDE_INT_TYPES_H
+#define INCLUDE_INT_TYPES_H
 
 typedef signed char             int8_t;
 typedef unsigned char           uint8_t;
-typedef signed int              int16_t;
-typedef unsigned int            uint16_t;
-typedef signed long int         int32_t;
-typedef unsigned long int       uint32_t;
-typedef signed long long int    int64_t;
-typedef unsigned long long int  uint64_t;
+typedef signed short            int16_t;
+typedef unsigned short          uint16_t;
+typedef signed int              int32_t;
+typedef unsigned int            uint32_t;
+typedef signed long             int64_t;
+typedef unsigned long           uint64_t;
 
-#endif /* ICLUDE_UINT_H */
+#endif // INCLUDE_INT_TYPES_H
