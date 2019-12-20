c63dd7f273e6fbe00d3af73b7d4588b3d9ec5ed1
furkanonder <furkantahaonder@gmail.com>
2019-12-20 17:20:49 +0300

    add include directory

--- src/build.sh
+++ src/build.sh
@@ -3,5 +3,7 @@ make -C asm
 cp asm/*.o obj/
 make -C sys
 cp sys/*.o obj/
+make -C include
+cp include/*.o obj/
 gcc -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c kernel_main.c -o obj/kernel_main.o
 ld -melf_i386 -nostdlib -O2 -T link.ld -o kernel.elf obj/*.o
