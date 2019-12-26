f7d1422f919070d02bca95ce53f681c06ef20928
furkanonder <furkantahaonder@gmail.com>
2019-12-26 17:02:32 +0300

    upgrade printing

--- src/include/tty.c
+++ src/include/tty.c
@@ -1,27 +1,69 @@
-#include "tty.h"
 #include "int_types.h"
+#include "tty.h"
+
+// VGA text buffer is located at physical memory address 0xB8000.
+volatile uint16_t* VGA_BUFFER = (uint16_t*)0xB8000;
+const uint8_t WHITE_COLOUR = 0x0F;
+
+struct term_struct *term_ptr,term_t;
 
 void clear_screen(){
-    char *vga = (char*)VGA_BUFFER;
-    uint16_t i=0;
-    
-    /* VGA mode 3 provides a text interface 80 characters 
-    wide and 25 characters lines per screen. */   
-    while( i < VGA_HEIGHT*VGA_WIDTH*2){
-        vga[i++] = 0;
-    }
+  uint16_t i=0;    
+  /* VGA mode 3 provides a text interface 80 characters 
+    wide and 25 characters lines per screen. */
+  while(i < VGA_HEIGHT*VGA_WIDTH*2){
+    VGA_BUFFER[i++] = 0;
+  }
 
 }
 
-void print(const char *string){
-    char *vga = (char*)VGA_BUFFER;
-    uint16_t i=0,j=0;
+void term_init(){
+  term_ptr = &term_t;
 
-    /*Text mode memory takes two bytes for every "character" on screen. 
-    One is the ASCII code byte, the other the attribute byte.*/    
-    while (string[j] != '\0') {
-        vga[i] = string[j++];
-        vga[i+1] = WHITE_COLOUR;
-        i += 2;
+  for(int col = 0; col < VGA_HEIGHT; col++){
+    for(int row = 0; row < VGA_WIDTH; row++){
+      const size_t index = (VGA_HEIGHT * term_ptr->row) + term_ptr->col;
+      // Set the character to blank
+      VGA_BUFFER[index] = ((uint16_t)WHITE_COLOUR << 8) | ' ';
     }
+  }
+
+}
+ 
+void put_char(char c){
+  term_ptr = &term_t;
+
+  switch (c){
+    // new line character
+    case '\n': {
+      term_ptr->col = 0;
+      term_ptr->row++;
+      break;
+    }
+ 
+    default:{
+      const size_t index = (VGA_HEIGHT * term_ptr->row) + term_ptr->col;
+      VGA_BUFFER[index] = ((uint16_t)WHITE_COLOUR << 8) | c;
+      term_ptr->col++;
+      break;
+    }
+  }
+
+  // New line
+  if(term_ptr->col >= VGA_HEIGHT){
+    term_ptr->col = 0;
+    term_ptr->row++;
+  }
+
+  // Reset screen
+  if(term_ptr->row >= VGA_WIDTH){
+    term_ptr->col = 0;
+    term_ptr->row = 0;
+  }
+
+}
+ 
+void print(const char* string){
+  for(size_t i=0; string[i] != '\0'; i++)
+    put_char(string[i]);
 }
--- src/include/tty.h
+++ src/include/tty.h
@@ -1,11 +1,13 @@
 #ifndef _TTY_H_
 #define _TTY_H_
 
-#define WHITE_COLOUR 0x7
-#define VGA_HEIGHT   80
-#define VGA_WIDTH    25
-// VGA text buffer is located at physical memory address 0xB8000.
-#define VGA_BUFFER   0xb8000
+#define VGA_HEIGHT	80
+#define VGA_WIDTH		25
+
+struct term_struct{
+  int col;
+  int row;
+};
 
 void clear_screen();
 void print(const char *string);
