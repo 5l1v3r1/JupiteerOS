006fa8168112caf0a619de6ec9595fe59ab1442a
furkanonder <furkantahaonder@gmail.com>
2020-03-23 22:34:26 +0300

    add uitoa and reverse_str functions

--- src/libc/string.c
+++ src/libc/string.c
@@ -3,9 +3,24 @@
 #include <libc/stdarg.h>
 #include <drivers/screen.h>
 
+// Calculate the length of a string
+int strlen(const char *s){
+  int i = 0;
+  while(s[i] != '\0') i++;
+  return i;
+}
+
+// Compare two string
+int strcmp(const char *s1, const char *s2){
+  for(; *s1==*s2; s1++,s2++)
+    if(*s1 == '\0')
+      return 0;
+  return *s1 - *s2;
+}
+
 /* The memset() function fills the first n bytes of the
  * memory area pointed to by s with the constant byte c.*/
-void *memset(void *s, int c, size_t n) {
+void *memset(void *s, int c, size_t n){
   char *m = (char *)s;
 
   for (size_t i = 0; i < n; i++)
@@ -14,28 +29,37 @@ void *memset(void *s, int c, size_t n) {
   return s;
 }
 
-// The itoa() function converts an integer to a string.
-char *itoa(int val, char *buf, int base) {
+void reverse_str(char s[]){
+  int c;
+  for(int i = 0, j = strlen(s) - 1; i < j; i++,j--){
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+// Converts an integer to a string.
+char *itoa(int val, char *buf, int base){
   static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-  char *str = buf, temp;
-  int sign;
+  char temp;
+  int sign,i = 0;
 
   // Sign
-  if ((sign = val) < 0)
+  if((sign = val) < 0)
     val = -val;
 
   // Convert
-  do {
-    *str++ = num[val % base];
-  } while (val /= base);
+  do{
+    buf[i++] = num[val % base];
+  }while(val /= base);
 
-  if (sign < 0)
-    *str++ = '-';
+  if(sign < 0)
+    buf[i++] = '-';
 
   // End of the string
-  *str = '\0';
+  buf = '\0';
 
-  char *end = str - 1;
+  char *end = buf - 1;
   char *begin = buf;
 
   // Reverse string
@@ -48,20 +72,36 @@ char *itoa(int val, char *buf, int base) {
   return buf;
 }
 
-int strlen(const char *s) {
-  int i = 0;
-
-  while (s[i] != '\0') i++;
+// Converts an unsigned integer to a string.
+char *uitoa(uint32_t val, char *buf, int base){
+  static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+  uint32_t i = 0;
+  uint32_t start = i;
   
-  return i;
-}
+  if(base == 16){
+    buf[i++] = '0';
+    buf[i++] = 'x';
+    start = i;
+  }
 
-// compare two string
-int strcmp(const char *s1, const char *s2){
-  for(; *s1==*s2; s1++,s2++)
-    if(*s1 == '\0')
-      return 0;
-  return *s1 - *s2;
+  // Convert
+  do{
+    buf[i++] = num[val % base];
+  }while(val /= base);
+
+  char *s = buf + start;
+  char *e = buf;
+
+  while(s < e){
+    char t = *s;
+    *s = *e;
+    *e = t;
+    s++; 
+    e--;
+  }
+
+  buf[i] = 0;
+  return buf;
 }
 
 int printf(const char *format, ...){
@@ -69,19 +109,18 @@ int printf(const char *format, ...){
   int val;
 
   /* ap is the object of va_list and it will hold the 
-   * information needed to retrieve the additional arguments with va_arg */
+   * information needed to retrieve the additional 
+   * arguments with va_arg */
   va_list ap;
 
-  va_start(ap, format);
+  va_start(ap,format);
   
   /* d,i => int; decimal number
    * x => int; unsigned hexadecimal number
    * c => int; single character
    * s => char *; print characters from the string until a '\0' 
-   * or the number of characters given by the precision.
-   */
-
-  for (int i=0; i < strlen(format); i++){
+   * or the number of characters given by the precision.*/
+  for(int i=0; i < strlen(format); i++){
     if (format[i] == '%'){
       i++;
       switch(format[i]){
@@ -92,12 +131,12 @@ int printf(const char *format, ...){
           break;
         case 'x':
           val = va_arg(ap, uint32_t);
-          itoa(val, buf, 16);
+          uitoa(val, buf, 16);
           print(buf);
           break;
         case 'd':
           val = va_arg(ap, uint32_t);
-          itoa(val, buf, 10);
+          uitoa(val, buf, 10);
           print(buf);
           break;
         case 'c':
@@ -112,7 +151,7 @@ int printf(const char *format, ...){
           print_char(format[i]);
       }
     } 
-    else {
+    else{
       print_char(format[i]);
     }
   }
